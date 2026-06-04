c2b3e1aca54be3c9ebf8c213b1eb3f4a701bb19d
RajaniN4805 <126142063+RajaniN4805@users.noreply.github.com>
2026-06-04 15:59:18 +0530

    Update 3_Geographical_Insights.py

--- pages/3_Geographical_Insights.py
+++ pages/3_Geographical_Insights.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+companies = pd.read_csv(
+    "data/objects.csv"
+)
+
+country = companies.groupby(
+    "country_code"
+).size().reset_index(name="count")
+
+fig = px.choropleth(
+    country,
+    locations="country_code",
+    color="count",
+    title="Startup Distribution"
+)
+
+st.plotly_chart(fig)
