7b597885498924b41541e81f40d86dd1c9f38ab4
RajaniN4805 <126142063+RajaniN4805@users.noreply.github.com>
2026-06-04 16:00:18 +0530

    Update 4_Startup_Explorer.py

--- pages/4_Startup_Explorer.py
+++ pages/4_Startup_Explorer.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
 
+companies = pd.read_csv(
+    "data/objects.csv"
+)
+
+startup = st.selectbox(
+    "Select Startup",
+    companies["name"].dropna()
+)
+
+data = companies[
+    companies["name"]==startup
+]
+
+st.dataframe(data.T)
