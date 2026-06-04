24562f60c4745016c346040a985ff0da28c3575e
RajaniN4805 <126142063+RajaniN4805@users.noreply.github.com>
2026-06-04 16:03:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Startup Funding Intelligence",
+    page_icon="🚀",
+    layout="wide"
+)
+
+st.title("🚀 Startup Funding Intelligence Dashboard")
+
+st.markdown("""
+Analyze Startup Ecosystem
+
+✔ Funding Trends
+✔ Acquisitions
+✔ IPOs
+✔ Investors
+✔ Machine Learning Predictions
+""")
