4f745aa15b1f4632d61ba7a08405c253431aca69
RajaniN4805 <126142063+RajaniN4805@users.noreply.github.com>
2026-06-04 15:58:25 +0530

    Update 2_Funding_Analytics.py

--- pages/2_Funding_Analytics.py
+++ pages/2_Funding_Analytics.py
@@ -1,1 +1,39 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+funding = pd.read_csv(
+    "data/funding_rounds.csv"
+)
+
+funding["raised_amount_usd"] = pd.to_numeric(
+    funding["raised_amount_usd"],
+    errors="coerce"
+)
+
+funding_type = funding.groupby(
+    "funding_round_type"
+)["raised_amount_usd"].sum().reset_index()
+
+fig = px.pie(
+    funding_type,
+    names="funding_round_type",
+    values="raised_amount_usd",
+    title="Funding Distribution"
+)
+
+st.plotly_chart(fig)
+
+top_rounds = funding.nlargest(
+    20,
+    "raised_amount_usd"
+)
+
+fig2 = px.bar(
+    top_rounds,
+    x="raised_amount_usd",
+    y="company_id",
+    orientation="h"
+)
+
+st.plotly_chart(fig2)
