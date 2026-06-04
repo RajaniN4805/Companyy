f1b0fb3f893788e67f783e7761b998c07ce1d4f0
RajaniN4805 <126142063+RajaniN4805@users.noreply.github.com>
2026-06-04 16:02:08 +0530

    Update 6_ML_Predictions.py

--- pages/6_ML_Predictions.py
+++ pages/6_ML_Predictions.py
@@ -1,1 +1,35 @@
+import streamlit as st
+import pandas as pd
 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("data/objects.csv")
+
+df = df[
+    ["relationships",
+     "milestones",
+     "funding_rounds",
+     "funding_total_usd"]
+]
+
+df = df.dropna()
+
+X = df.drop(
+    "funding_total_usd",
+    axis=1
+)
+
+y = df["funding_total_usd"]
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y,test_size=0.2
+)
+
+model = RandomForestRegressor()
+
+model.fit(X_train,y_train)
+
+st.success(
+    f"Model Score: {model.score(X_test,y_test):.2f}"
+)
