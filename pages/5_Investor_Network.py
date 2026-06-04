791cb4292164b7012b159e54b66bf72150f26eb8
RajaniN4805 <126142063+RajaniN4805@users.noreply.github.com>
2026-06-04 16:01:15 +0530

    Update 5_Investor_Network.py

--- pages/5_Investor_Network.py
+++ pages/5_Investor_Network.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+investments = pd.read_csv(
+    "data/investments.csv"
+)
+
+top = investments.groupby(
+    "funding_round_id"
+).size().reset_index(name="investments")
+
+fig = px.histogram(
+    top,
+    x="investments",
+    nbins=40
+)
+
+st.plotly_chart(fig)
